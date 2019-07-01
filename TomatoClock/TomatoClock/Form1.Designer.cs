5ecb93f16dc859814b903b1241845079807cc399
earthmanylf <43513215+earthmanylf@users.noreply.github.com>
2019-07-01 19:44:47 +0800

    Add files via upload

---
+++ TomatoClock/TomatoClock/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace TomatoClock
+{
+    partial class TomatoClock
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TomatoClock));
+            this.Start = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.init = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(105, 12);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(87, 30);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "开始计时";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(105, 48);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(87, 30);
+            this.Pause.TabIndex = 1;
+            this.Pause.Text = "暂停计时";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // init
+            // 
+            this.init.Location = new System.Drawing.Point(105, 84);
+            this.init.Name = "init";
+            this.init.Size = new System.Drawing.Size(87, 30);
+            this.init.TabIndex = 2;
+            this.init.Text = "清零";
+            this.init.UseVisualStyleBackColor = true;
+            this.init.Click += new System.EventHandler(this.init_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(1, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "00m00s";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(1, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "设定时间：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 132);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(59, 25);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(157, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "min";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TomatoClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(214, 187);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.init);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TomatoClock";
+            this.Text = "TomatoClock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button init;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TomatoClock/TomatoClock/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+namespace TomatoClock
+{
+    public partial class TomatoClock : Form
+    {
+        #region 调用外部dll
+        [DllImport("user32.dll", EntryPoint = "SetForegroundWindow")]
+        //设置窗体句柄的窗体为活动窗体，因为如果此程序并不在焦点中，可以使此程序回归活动窗体
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        #endregion
+        private int timer;//用户指定的倒计时
+        private bool is_run = false;//控制番茄钟是否在运行
+        int min;
+        int sec;
+        public TomatoClock()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            timer = Convert.ToInt32(textBox1.Text) * 60;
+            timer1.Enabled = true;
+            is_run = true;
+            Pause.Text = "暂停计时";
+        }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            if (Pause.Text == "暂停计时")
+            {
+                timer1.Stop();
+                is_run = false;
+                Pause.Text = "恢复计时";
+            }
+            else
+            {
+                timer1.Start();
+                is_run = true;
+                Pause.Text = "暂停计时";
+            }
+        }
+
+        private void init_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            Pause.Text = "暂停计时";
+            label1.Text = "00m00s";
+            timer = 0;
+            is_run = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timer > 0)
+            {
+                timer--;
+                min = timer / 60;
+                sec = timer % 60;
+                label1.Text = min.ToString() + 'm' + sec.ToString() + 's';
+            }
+            else
+            {
+                timer1.Enabled = false;
+                SetForegroundWindow(this.Handle);
+                MessageBox.Show("TIME TO TAKE A REST!");
+            }
+        }
+    }
+}
